6bcee1d34fe5e47e84c2cf1ed16e9a879b051d26
[LIAM] <liam@axanite.net>
2015-12-20 22:05:03 +0000

    Added plugin.yml

--- src/uk/intristicmc/core/IntristicCORE.java
+++ src/uk/intristicmc/core/IntristicCORE.java
@@ -2,8 +2,10 @@ package uk.intristicmc.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import uk.intristicmc.core.commands.CMDChatmode;
+
 public class IntristicCORE extends JavaPlugin {
 	public void onEnable() {
-		
+		getCommand("chatmode").setExecutor(new CMDChatmode());
 	}
 }
--- src/uk/intristicmc/core/miscellaneous/Utils.java
+++ src/uk/intristicmc/core/miscellaneous/Utils.java
@@ -2,10 +2,22 @@ package uk.intristicmc.core.miscellaneous;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.ChatColor;
 
 public class Utils {
-
 	public static Plugin getInstance() {
 		return Bukkit.getPluginManager().getPlugin("IntristicCORE");
+	}
+	
+	public static String getPrefix() {
+		String s = "&8[&cIntristic&4CORE&8]";
+		s = ChatColor.translateAlternateColorCodes('&', s);
+		return s;
+	}
+	
+	public static String getNoPermissionMessage(String node) {
+		String s = getPrefix() + "&cYou're lacking the permission node: " + node;
+		s = ChatColor.translateAlternateColorCodes('&', s);
+		return s;
 	}
 }
