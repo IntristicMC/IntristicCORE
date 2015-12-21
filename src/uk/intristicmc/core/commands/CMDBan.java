e69d45686599c4c7feea93ceea6d33ef88b35ed2
paul-zircon <goldenpaul27@sky.com>
2015-12-21 11:11:17 +0000

    Updated MySQLHandler
    uk.intristicmc.core.miscellaneous.MySQLHandler added.

--- src/uk/intristicmc/core/commands/CMDBan.java
+++ src/uk/intristicmc/core/commands/CMDBan.java
@@ -4,9 +4,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import uk.intristicmc.core.miscellaneous.Utils;
+
 public class CMDBan implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("ban")) {
+			if(!sender.hasPermission("intristicmc.core.ban")) {
+				sender.sendMessage(Utils.getNoPermissionMessage("intristicmc.core.ban"));
+				return true;
+			}
 			
 		}
 		return true;
--- src/uk/intristicmc/core/miscellaneous/Utils.java
+++ src/uk/intristicmc/core/miscellaneous/Utils.java
@@ -1,6 +1,7 @@
 package uk.intristicmc.core.miscellaneous;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.Plugin;
 
 public class Utils {
@@ -8,4 +9,16 @@ public class Utils {
 	public static Plugin getInstance() {
 		return Bukkit.getPluginManager().getPlugin("IntristicCORE");
 	}
+	
+	public static String getPrefix() {
+        String s = "&8[&cIntristic&4CORE&8]";
+        s = ChatColor.translateAlternateColorCodes('&', s);
+        return s;
+    }
+	
+	public static String getNoPermissionMessage(String node) {
+        String s = getPrefix() + "&cYou're lacking the permission node: " + node;
+        s = ChatColor.translateAlternateColorCodes('&', s);
+        return s;
+    }
 }
