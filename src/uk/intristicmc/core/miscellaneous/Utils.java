545da70232e428ad5a149ce776d753b48cf666b0
paul-zircon <goldenpaul27@sky.com>
2015-12-21 11:14:23 +0000

    Utils Method Fix
    Duplicate method inside uk.intristicmc.core.miscellaneous.utils

--- src/uk/intristicmc/core/miscellaneous/Utils.java
+++ src/uk/intristicmc/core/miscellaneous/Utils.java
@@ -3,7 +3,6 @@ package uk.intristicmc.core.miscellaneous;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.ChatColor;
 
 public class Utils {
 	public static Plugin getInstance() {
@@ -11,7 +10,6 @@ public class Utils {
 	}
 	
 	public static String getPrefix() {
-<<<<<<< HEAD
         String s = "&8[&cIntristic&4CORE&8]";
         s = ChatColor.translateAlternateColorCodes('&', s);
         return s;
@@ -22,16 +20,4 @@ public class Utils {
         s = ChatColor.translateAlternateColorCodes('&', s);
         return s;
     }
-=======
-		String s = "&8[&cIntristic&4CORE&8]";
-		s = ChatColor.translateAlternateColorCodes('&', s);
-		return s;
-	}
-	
-	public static String getNoPermissionMessage(String node) {
-		String s = getPrefix() + "&cYou're lacking the permission node: " + node;
-		s = ChatColor.translateAlternateColorCodes('&', s);
-		return s;
-	}
->>>>>>> origin/master
 }
